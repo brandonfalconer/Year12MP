c766827d4817f455d6219a0fac47dd670ef7ac21
Brandon <brandon.falconer1@gmail.com>
2018-08-02 20:19:59 +1000

    Basic pygame template

--- Main.py
+++ Main.py
@@ -1,2 +1,63 @@
-import pygame 
+# --- Import Libraries
+import pygame
+import sys
+import random
 
+# --- Define global constants
+# Define some colors
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255,255,0)
+PURPLE = (255, 0, 255)
+CYAN = (0,255,255)
+WHITE = (255, 255, 255)
+
+# Screen dimensions
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# --- Define classes
+
+# --- Define functions
+
+
+# --- MAIN PROGRAM
+# Initialise pygame
+pygame.init()
+
+# Initialise screen properties
+screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+pygame.display.set_caption("Who Wants to be a Millionaire?")
+
+# Loop until the user clicks the close button.
+done = False
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+# -------- Main Program -----------
+# Variables
+
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    # --- Game logic
+
+    # --- Screen-clearing
+    screen.fill(WHITE)
+
+    # --- Drawing code
+
+    # --- Update the screen
+    pygame.display.flip()
+
+    # --- Set to 60 frames per second
+    clock.tick(60)
+
+# Close the window and quit.
+pygame.quit()
