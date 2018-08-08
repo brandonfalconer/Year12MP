14c48a2e927c92e45df98d8e326ae56714778d17
brandonfalconer <brandon.falconer1@gmail.com>
2018-08-08 12:30:05 +1000

    Assets

---
+++ ButtonLibrary.py
@@ -0,0 +1,25 @@
+import pygame
+
+# Variables
+screen = pygame.display.set_mode([1280, 720])
+pygame.font.init()
+font = pygame.font.Font("Assets/Fonts/Tahoma.ttf", 30)
+
+
+class GameButton:
+    def __init__(self, x, y, width, height, colour, text, pressed):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.colour = colour
+        self.text = text
+        self.pressed = pressed
+
+    def draw(self):
+        # Drawing a rectangle
+        pygame.draw.rect(screen, self.colour, (self.x, self.y, self.width, self.height))
+
+        # Drawing Text
+        text = font.render(self.text, True, (0, 0, 0))
+        screen.blit(text, (self.x + 50, (self.y + self.height / 2 - 10)))
--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 # --- Import Libraries
 import pygame
 import random
-import Button_Library
+import ButtonLibrary
 import re
 
 # --- Define global constants
@@ -20,6 +20,9 @@ SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
 # --- Define classes
+#class GameState:
+    #def __init__(self, state):
+        #self.state = state
 
 # --- Define functions
 def split_line(line):
@@ -41,33 +44,44 @@ clock = pygame.time.Clock()
 
 # -------- Main Program -----------
 # Variables
-new_button = Button_Library.GameButton(100, 100, 200, 200, BLUE, "Start", False)
-
 question_info = open("QuestionInfo.txt")
 line_number = 0
+
+buttons = []
+start_button = ButtonLibrary.GameButton(100, 100, 200, 200, BLUE, "Start", False)
+buttons.append(start_button)
 
 for line in question_info:
     words = split_line(line)
     line_number += 1
 
     for word in words:
-        i = 0
         i = 1
-
 
 while not done:
     # --- Main event loop
+    # Gather user input
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
+        if event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                mouse_x = pygame.mouse.get_pos()[0]
+                mouse_y = pygame.mouse.get_pos()[1]
+                for button in buttons:
+                    if (mouse_x > button.x and mouse_x < button.x + button.width) and (mouse_y > button.y and mouse_y < button.y + button.height):
+                        button.pressed = True
+
     # --- Game logic
+    #if start_button.pressed:
+
 
     # --- Screen-clearing
     screen.fill(WHITE)
 
     # --- Drawing code
-    new_button.update()
+    start_button.draw()
 
     # --- Update the screen
     pygame.display.flip()
